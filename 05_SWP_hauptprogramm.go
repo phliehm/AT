f93fe6386b7a2f4f259ad99e87e604870b785440
Philipp <philipp.liehm@gmail.com>
2023-03-13 11:40:58 +0100

    Jetzt gehts

---
+++ 05_SWP_hauptprogramm.go
@@ -0,0 +1,12 @@
+// Autor: F. Möbius
+// Zweck: VL SWP, Arbeiten mit git
+package main
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("Das Hauptprogramm ist gestartet.")
+	fmt.Println("Das Hauptprogramm ist beendet.")
+}
+
